b8c64512436edee7712b2f42af7cbea94cc624b8
sandrocods <59155826+sandrocods@users.noreply.github.com>
2022-11-08 09:06:41 +0700

    update google ads

--- src/process.py
+++ src/process.py
@@ -16,18 +16,7 @@ class ZefoyViews:
 
     STATIC_HEADERS = {
         "Host": "zefoy.com",
-        "Connection": "keep-alive",
-        "sec-ch-ua": "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-User": "?1",
-        "Sec-Fetch-Dest": "document",
-        "Accept-Language": "en-US,en;q=0.9",
         "x-requested-with": "XMLHttpRequest",
         "origin": "https://zefoy.com",
     }
@@ -43,7 +32,6 @@ class ZefoyViews:
         self.key_views = None
         self.session = requests.Session()
         self.google_ads_inject()
-
         self.captcha = None
         self.phpsessid = None
 
